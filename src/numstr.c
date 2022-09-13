0f15a0106b4b3f513b4627b00a538a63ffbd8ba5
Andy Heck <aheck@wavetrix.com>
2022-09-12 21:16:35 -0500

    #7: starts implementing numstr_divide_int()
    * Adds numstr_divide_int()
    * Adds tests for numstr_divide_int() which work for up to base 36
    * Paused pending #8 fix for higher bases

--- src/numstr.c
+++ src/numstr.c
@@ -3,10 +3,12 @@
 
 #include "numstr_internal.h"
 
-
-numstr_ret_t numstr_fill_from_str(numstr_t *num, char* str, size_t str_size, uint8_t base) {
+numstr_ret_t numstr_fill_from_str(numstr_t *num, char *str, size_t str_size, uint8_t base)
+{
     numstr_ret_t retcode;
-    if(NULL == num || NULL == str) {
+    int result;
+    if (NULL == num || NULL == str)
+    {
         return NUMSTR_RET_ERR_NULL_PTR;
     }
 
@@ -16,24 +18,77 @@ numstr_ret_t numstr_fill_from_str(numstr_t *num, char* str, size_t str_size, uin
     num->str = str;
     num->str_size = str_size;
 
-    NSI_RET_ON_ERR(nsi_str_is_valid(num));
+    NSI_RET_ON_ERR(result = nsi_str_is_valid(num));
+
+    num->digit_count = (size_t)result;
 
     return NUMSTR_RET_SUCCESS;
 }
 
-numstr_ret_t numstr_fill_as_empty(numstr_t *num, char *str, size_t str_size) {
+numstr_ret_t numstr_fill_as_empty(numstr_t *num, char *str, size_t str_size)
+{
     numstr_ret_t retcode;
-    if(NULL == num || NULL == str) {
+    if (NULL == num || NULL == str)
+    {
         return NUMSTR_RET_ERR_NULL_PTR;
     }
-    
-    num->base = 0;
+
     num->str = str;
     num->str_size = str_size;
 
-    // make the first and last char null
-    num->str[0] = '\0';
-    num->str[str_size - 1] = '\0';
+    nsi_set_empty(num);
 
     return NUMSTR_RET_SUCCESS;
+}
+
+numstr_ret_t numstr_divide_int(numstr_t *num, int divisor, numstr_t *result, int *remainder)
+{
+    int64_t r;
+    int i;
+
+    if (NULL == num || NULL == result || NULL == remainder)
+    {
+        return NUMSTR_RET_ERR_NULL_PTR;
+    }
+
+    // configure empty result
+    nsi_set_empty(result);
+    
+    // Divide each digit, and carry the remainder forward to the next digit
+    for(i = 0, r = 0; i < numstr_digit_count(num); ++i) {
+        int digit_val;
+        int new_digit_val;
+        
+        digit_val = nsi_char_to_value(num->str[i]);
+        NSI_RET_ON_TRUE(digit_val < 0, NUMSTR_RET_ERR_INVALID_CHAR);
+        r = num->base * r + digit_val;
+        new_digit_val = r / divisor;
+        r %= divisor;
+
+        // Add new digits as needed
+        if(new_digit_val > 0 || !nsi_is_empty(result)) {
+            char new_digit;
+            result->base = num->base;
+            new_digit = nsi_value_to_char(new_digit_val);
+            NSI_RET_ON_TRUE( '\0' == new_digit, NUMSTR_RET_ERR_INVALID_CHAR);
+            NSI_RET_ON_ERR(nsi_append_digit(result, new_digit));
+        }
+    }
+
+    if(nsi_is_empty(result)) {
+        NSI_RET_ON_ERR(nsi_append_digit(result, '0'));
+    }
+
+    *remainder = (int)r;
+    return NUMSTR_RET_SUCCESS;
+}
+
+int numstr_digit_count(numstr_t *num)
+{
+    if (NULL == num)
+    {
+        return NUMSTR_RET_ERR_NULL_PTR;
+    }
+
+    return num->digit_count;
 }
--- src/numstr_internal.c
+++ src/numstr_internal.c
@@ -14,7 +14,9 @@
 
 bool nsi_char_is_valid_for_base(char c, uint8_t base)
 {
-    return nsi_char_to_value(c) < base;
+    int retcode;
+    retcode = nsi_char_to_value(c);
+    return (retcode > -1) && (retcode < base);
 }
 
 bool nsi_base_is_valid(uint8_t base)
@@ -27,17 +29,58 @@ numstr_ret_t nsi_str_is_valid(numstr_t *num)
     int i;
 
     // First, look for at least 1 null terminator
-    for(i = num->str_size - 1;
-        i >= 0 && num->str[i] != '\0';
-        --i);
+    for (i = num->str_size - 1;
+         i >= 0 && num->str[i] != '\0';
+         --i)
+        ;
 
     NSI_RET_ON_TRUE(i < 0, NUMSTR_RET_ERR_STR_MISSING_NULL);
 
     // There is a null terminator!
     // Start from the front and check that the string contains only valid characters
-    for(i = 0; num->str[i] != '\0'; ++i) {
-        NSI_RET_ON_FALSE(nsi_char_is_valid_for_base(num->str[i], num->base), NUMSTR_RET_ERR_INVALID_CHAR_FOR_BASE);
+    for (i = 0; num->str[i] != '\0'; ++i)
+    {
+        if(!nsi_char_is_valid_for_base(num->str[i], num->base)) {
+            return NUMSTR_RET_ERR_INVALID_CHAR_FOR_BASE;
+        }
     }
+
+    return i;
+}
+
+void nsi_set_empty(numstr_t *num)
+{
+    num->base = 0;
+    num->str[0] = '\0';
+    num->str[num->str_size - 1] = '\0';
+    num->digit_count = 0;
+}
+
+bool nsi_is_empty(numstr_t *num)
+{
+    return (0 == num->base) && (0 == num->digit_count) && ('\0' == num->str[0]) && ('\0' == num->str[num->str_size - 1]);
+}
+
+int nsi_empty_digit_count(numstr_t *num)
+{
+    int digit_count;
+    NSI_RET_ON_ERR(digit_count = numstr_digit_count(num));
+    return num->str_size - digit_count - 1;
+}
+
+numstr_ret_t nsi_append_digit(numstr_t *num, char digit)
+{
+    int digit_count;
+
+    NSI_RET_ON_FALSE(nsi_char_is_valid_for_base(digit, num->base), NUMSTR_RET_ERR_INVALID_CHAR_FOR_BASE);
+
+    NSI_RET_ON_FALSE(nsi_empty_digit_count(num) > 0, NUMSTR_RET_ERR_NO_SPACE);
+
+    NSI_RET_ON_ERR(digit_count = numstr_digit_count(num));
+
+    num->str[digit_count] = digit;
+    num->str[digit_count + 1] = '\0';
+    num->digit_count++;
 
     return NUMSTR_RET_SUCCESS;
 }
